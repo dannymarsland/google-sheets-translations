19d88ca594e30ada21ded99c7669e9fba9e15b25
Danny Marsland <danny.marsland@gmail.com>
2016-06-24 02:04:59 +0100

    - editorconfig file - tabs replaced by spaces

--- worksheet-translations.js
+++ worksheet-translations.js
@@ -1,28 +1,28 @@
 function WorksheetTranslations(title, translations, locales) {
-	var self = this;
-	self.locales = locales;
-	self.translations = translations;
-	self.title = title;
+    var self = this;
+    self.locales = locales;
+    self.translations = translations;
+    self.title = title;
 
-	self.getTitle = function() {
-		return self.title;
-	};
+    self.getTitle = function() {
+        return self.title;
+    };
 
-	self.getLocales = function() {
-		return self.locales;
-	};
+    self.getLocales = function() {
+        return self.locales;
+    };
 
-	self.getTranslationsForToken = function(token) {
-		return self.translations[token] || {};
-	};
+    self.getTranslationsForToken = function(token) {
+        return self.translations[token] || {};
+    };
 
-	self.getTranslationsForLocale = function(locale) {
-		var localeTranslations = {};
-		Object.keys(self.translations).forEach(function(token) {
-			localeTranslations[token] = self.translations[token][locale];
-		});
-		return localeTranslations;
-	};
+    self.getTranslationsForLocale = function(locale) {
+        var localeTranslations = {};
+        Object.keys(self.translations).forEach(function(token) {
+            localeTranslations[token] = self.translations[token][locale];
+        });
+        return localeTranslations;
+    };
 }
 
 module.exports = WorksheetTranslations;
